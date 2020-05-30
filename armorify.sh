efa3240e101db40b5c1b52a2c1ee0577843de182
Alex Belanger <nitrix@nitrix.me>
2020-05-30 04:47:30 -0400

    Armorifying with Go

--- armorify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cat $1 |
-tr -d " " |
-sed "s/-----BEGINCERTIFICATE-----/-----BEGIN CERTIFICATE-----\\n/" |
-sed "s/-----ENDCERTIFICATE-----/\\n-----END CERTIFICATE-----/" |
-sed "s/-----BEGINRSAPRIVATEKEY-----/-----BEGIN RSA PRIVATE KEY-----\\n/" |
-sed "s/-----ENDRSAPRIVATEKEY-----/\\n-----END RSA PRIVATE KEY-----/" > $1
