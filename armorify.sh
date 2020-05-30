53116dcbc5574e899398e9f91f19858f1c8733c1
Alex Belanger <nitrix@nitrix.me>
2020-05-30 00:48:33 -0400

    What if it works

--- armorify.sh
+++ armorify.sh
@@ -2,4 +2,6 @@
 cat $1 |
 tr -d " " |
 sed "s/-----BEGINCERTIFICATE-----/-----BEGIN CERTIFICATE-----\\n/" |
-sed "s/-----ENDCERTIFICATE-----/\\n-----END CERTIFICATE-----/" > $1
+sed "s/-----ENDCERTIFICATE-----/\\n-----END CERTIFICATE-----/" |
+sed "s/-----BEGINRSAPRIVATEKEY-----/-----BEGIN RSA PRIVATE KEY-----\\n/" |
+sed "s/-----ENDRSAPRIVATEKEY-----/\\n-----END RSA PRIVATE KEY-----/" > $1
